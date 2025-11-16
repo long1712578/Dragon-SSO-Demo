481081636e31b0c28850f48057456016d751e8d7
Long Pham <55705988+long1712578@users.noreply.github.com>
2025-11-16 16:58:32 +0700

    Add Program.cs for Identity Service API

---
+++ src/IdentityService.API/Program.cs
@@ -0,0 +1,35 @@
+using IdentityService.API;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Async(c => c.File("Logs/logs.txt"))
+    .WriteTo.Async(c => c.Console())
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting Dragon SSO - Identity Service");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host
+        .AddAppSettingsSecretsJson()
+        .UseAutofac()
+        .UseSerilog();
+
+    await builder.AddApplicationAsync<IdentityServiceModule>();
+    var app = builder.Build();
+    await app.InitializeApplicationAsync();
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Identity Service terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
